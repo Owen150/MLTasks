f21ebc2735584f9662582850740cd2f70c958676
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-06-20 23:34:13 +0300

    Quiz Game

---
+++ quiz_game.py
@@ -0,0 +1,17 @@
+print("Welcome to my Computer Quiz")
+
+playing = input("Do you want to play? ")
+if playing.lower() != "yes":
+    quit()
+        
+print("Okay, Let's Play")
+score = 0
+
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+print("You got " + str(score) + " questions correct")
