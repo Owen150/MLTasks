f22f33e72c666df161945447169681c055ca87d7
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-06-22 22:19:39 +0300

    Encrypting/Hashing the Passwords

--- password_manager.py
+++ password_manager.py
@@ -1,4 +1,22 @@
+from cryptography.fernet import Fernet
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+'''
+# After you write the key in the file once, comment out the function
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
 master_pwd = input("Enter the Master Password: ")
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
 
 def view():
     with open("passwords.txt", "r") as f:
@@ -12,7 +30,7 @@ def add():
     pwd = input("Account Password: ")
     
     with open("passwords.txt", "a") as f:
-        f.write(name + "|" + pwd + "\n") 
+        f.write(name + "|" + str(fer.encrypt(pwd.encode())) + "\n") 
 
 while True:
     mode = input("Would you like to Add a new password or View an existing password (add/view), or type in q to quit?: ")
@@ -26,6 +44,6 @@ while True:
         add()
         
     else:
-        print("Invalid mode.")
+        print("Invalid mode. Please try again.")
         continue
 print("End")
