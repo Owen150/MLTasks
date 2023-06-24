6ea41d53e335b664eb26923144b0922429acf357
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-06-24 20:54:36 +0300

    Get Bet amount from the user

--- slot_machine.py
+++ slot_machine.py
@@ -1,4 +1,6 @@
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 def deposit():
     while True:
@@ -26,9 +28,25 @@ def get_number_of_lines():
             print("Please enter a number.")
     return lines
 
+def get_bet():
+    while True:
+        amount = input("How much would you like to bet on each line?: $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be within ${MIN_BET} - ${MAX_BET}.")
+        else:
+            print("Please enter a number.")
+    return amount
+
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance, lines)
+    bet = get_bet()
+    total_bet = bet * lines
     
+    print(f"You are betting ${bet} on {lines} lines. Total bet is ${total_bet}.")
+        
 main()
