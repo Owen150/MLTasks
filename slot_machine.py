7a498ed22d6c9eafcbbf2cc489c8041b36879e90
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-07-01 18:31:08 +0300

    Re-factoring Code - Slot Machine Complete

--- slot_machine.py
+++ slot_machine.py
@@ -109,8 +109,7 @@ def get_bet():
             print("Please enter a number.")
     return amount
 
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
@@ -126,6 +125,19 @@ def main():
     print_slot_machine(slots)
     winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
     print(f"You won ${winnings}.")
-    print(f"You won on lines: ", *winning_lines)
+    print(f"You won on lines:", *winning_lines)
+    return winnings - total_bet
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is ${balance}")
+        answer = input("Press enter to play (q to quit): ")
+        if answer == "q":
+            break
+        balance += spin(balance)
+    
+    print(f"You are left with ${balance}")
+    
     
 main()
