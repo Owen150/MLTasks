96daf6cabd2b6fddf7d4462144014b350552c834
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-07-03 17:45:29 +0300

    Printing out the Maze

--- path_finder.py
+++ path_finder.py
@@ -15,10 +15,23 @@ maze = [
     ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
 ]
 
+def print_maze(maze, stdscr, path=[]):
+    BLUE = curses.color_pair(1)
+    RED = curses.color_pair(2)
+    
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            stdscr.addstr(i, j*2, value, BLUE)
+
 def main(stdscr):
+    #Initializing the foreground and background
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+
     stdscr.clear()
-    stdscr.addstr(0, 0, "Hello World!")
+    print_maze(maze, stdscr)
     stdscr.refresh()
     stdscr.getch()
+
     
 wrapper(main)
