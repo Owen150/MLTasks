e67b5ba68562ef721422a0a1a81683a502a7fb23
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-07-06 02:21:53 +0300

    Getting Individual Team Stats

--- nba_scores.py
+++ nba_scores.py
@@ -22,7 +22,22 @@ def get_scoreboard():
         period = game['period']
 
         print("----------------------------------------------")
-        print(f"{home_team} vs {away_team}, {clock}, {period}")
+        print(f"{home_team['triCode']} vs {away_team['triCode']}")
+        print(f"{home_team['score']} - {away_team['score']}")
+        print(f"{clock} - {period['current']}")
+        
+def get_stats():
+    stats = get_links()['leagueTeamStatsLeaders']
+    teams = get(BASE_URL + stats).json()['league']['standard']['regularSeason']['teams']
+    
+    teams = list(filter(lambda x: x['name'] != 'Team', teams))
+    teams.sort(key=lambda x: int(x['ppg']['rank']))
+    
+    for i, team in enumerate(teams):
+        name = team['name']
+        nickname = team['nickname']
+        ppg = team['ppg']
+        print(f"{i + 1}. {name} - {nickname} - {ppg}")
 
 
-get_scoreboard()
+get_stats()
