28839357af22d8dde82106da3a124d9c83edbcf2
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-07-07 22:49:35 +0300

    Defining the Exchange Rate for Currencies

--- currency_converter.py
+++ currency_converter.py
@@ -12,7 +12,7 @@ def get_currencies():
     data = get(url).json()['results']
     
     #Converting the data from a dictionary to a list by getting all the items so that we can sort the data by the different currency names
-    #The items will give tuples that contain the key-the currency, value-associated with each currency
+    #The items will give tuples that contain the key-the currencyName, value-associated with each currency
     data = list(data.items())
     #Will be sorted by the first item in the set/list i.e. currencyName
     data.sort()
@@ -26,6 +26,14 @@ def print_currencies(currencies):
         symbol = currency.get("currencySymbol", "")
         print(f"{_id} - {name} - {symbol}")
         
+def exchange_rate(currency1, currency2):
+    endpoint = f"api/v7/convert?q={currency1}_{currency2}&compact=ultra&apiKey={API_KEY}"
+    url = BASE_URL + endpoint
+    response = get(url)
+    data = response.json()
+    printer.pprint(data)
+        
 
-data = get_currencies()
-print_currencies(data)
+#data = get_currencies()
+#print_currencies(data)
+exchange_rate("USD", "CAD")
