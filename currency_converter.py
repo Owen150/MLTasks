25aaab9a1d4805751f2ae4784eb2c716ab30d3bd
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-07-08 17:56:57 +0300

    Updates

--- currency_converter.py
+++ currency_converter.py
@@ -14,7 +14,7 @@ def get_currencies():
     #Converting the data from a dictionary to a list by getting all the items so that we can sort the data by the different currency names
     #The items will give tuples that contain the key-the currencyName, value-associated with each currency
     data = list(data.items())
-    #Will be sorted by the first item in the set/list i.e. currencyName
+    #Will be sorted by the first item in the tuple i.e. currencyName
     data.sort()
     
     return data
