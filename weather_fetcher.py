58282cc49cc3c353dd927c97d1f57b2f2c866fa1
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-07-10 18:42:58 +0300

    Updates

--- weather_fetcher.py
+++ weather_fetcher.py
@@ -1,11 +1,14 @@
-import requests
+from requests import get
+from pprint import PrettyPrinter
 
 API_KEY = "ec9423d44b4ef60dfa92767ba50a71c8"
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
+printer = PrettyPrinter()
+
 city = input("Enter a city name: ")
 request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
-response = requests.get(request_url)
+response = get(request_url)
 
 if response.status_code == 200:
     data = response.json()
